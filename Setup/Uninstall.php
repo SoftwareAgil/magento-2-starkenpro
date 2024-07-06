86b6e6d23fb93fdb656d128c280c614f9c500d29
root <root@Miguel>
2024-07-05 20:11:00 -0500

    Ajustes uninstall composer

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -2,11 +2,11 @@
 
 namespace SoftwareAgil\StarkenPro\Setup;
 
-use Magento\Framework\Setup\UninstallSchemaInterface;
+use Magento\Framework\Setup\UninstallInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 
-class UninstallSchema implements UninstallSchemaInterface
+class Uninstall implements UninstallInterface
 {
     public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
